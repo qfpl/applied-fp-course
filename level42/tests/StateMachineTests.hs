805b87f75211b3423b44fa951967bbc45638d80a
Andrew McCluskey <andrew@ajmccluskey.com>
2018-02-16 22:59:36 +1000

    Start fleshing out state machine tests
    Can use an MVar with `race_` to trigger a shutdown of warp via asynchronous
    exceptions.

--- level42/tests/StateMachineTests.hs
+++ level42/tests/StateMachineTests.hs
@@ -1,18 +1,40 @@
 module StateMachineTests where
 
-import qualified Data.IntMap    as M
-import           Data.Text      (Text)
+import Control.Concurrent.Async (race_)
+import           Control.Concurrent.MVar  (MVar, newEmptyMVar, putMVar, takeMVar)
+import           Control.Monad.IO.Class   (liftIO)
+import qualified Data.IntMap              as M
+import           Data.Semigroup           ((<>))
+import           Data.Text                (Text)
+import           Data.Text.IO             (hPutStrLn)
+import           GHC.Word                 (Word16)
+import           Network.Wai.Handler.Warp (run)
+import           System.IO                (stderr)
 
-import           Hedgehog       (Command)
-import qualified Hedgehog.Gen   as Gen
-import qualified Hedgehog.Range as Range
+import           Hedgehog                 (Command)
+import qualified Hedgehog.Gen             as Gen
+import qualified Hedgehog.Range           as Range
 
-import           FirstApp.Types (Conf (Conf), DBFilePath (DBFilePath),
-                                 Port (Port))
+import           FirstApp.AppM            (Env (Env))
+import           FirstApp.DB              (initDB)
+import           FirstApp.Types           (Conf (Conf), DBFilePath (DBFilePath),
+                                           Port (Port))
 
 main :: IO ()
-main =
-  putStrLn "Hello, State Machine Tests"
+main = do
+  env' <- env
+  let app' = app env
+  run (fromIntegral portNum) app'
+
+runUntilSignalled
+  :: IO a
+  -> MVar ()
+  -> IO ()
+runUntilSignalled a m =
+  race_ a (takeMVar m)
+
+portNum :: Word16
+portNum = 3000
 
 data Comment =
   Comment { topic   :: Text
@@ -21,8 +43,16 @@ data Comment =
 
 type CommentState = M.IntMap Comment
 
-config :: Conf
-config = Conf (Port 3000) (DBFilePath "state-machine-tests.sqlite")
+env :: IO Env
+env =
+  let
+    dbPath = DBFilePath "state-machine-tests.sqlite"
+    c = Conf (Port 3000) dbPath
+    edb = initDB dbPath
+    logErr = liftIO . hPutStrLn stderr
+    splode = error . ("Error connecting to DB: " <>) . show
+  in
+    fmap (either splode (Env logErr c)) edb
 
 initialState :: CommentState
 initialState =
