5afdb5d0372e4680d62a50bd1a1e2d8e65ff1c43
Andrew McCluskey <andrew@ajmccluskey.com>
2018-02-16 16:25:23 +1000

    Make compiling

--- level42/level42.nix
+++ level42/level42.nix
@@ -1,22 +1,23 @@
-{ mkDerivation, aeson, base, bytestring, doctest, hspec, hspec-wai
-, http-types, mtl, optparse-applicative, sqlite-simple, semigroups
-, sqlite-simple-errors, stdenv, text, time, wai, wai-extra, warp
-, transformers
+{ mkDerivation, aeson, base, bytestring, containers, doctest
+, hedgehog, hspec, hspec-wai, http-types, mtl, optparse-applicative
+, semigroups, sqlite-simple, sqlite-simple-errors, stdenv, text
+, time, transformers, wai, wai-extra, warp
 }:
 mkDerivation {
-  pname = "level07";
+  pname = "level42";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     aeson base bytestring http-types mtl optparse-applicative
-    sqlite-simple sqlite-simple-errors text time wai warp semigroups
-    transformers
+    semigroups sqlite-simple sqlite-simple-errors text time
+    transformers wai warp
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    base bytestring doctest hspec hspec-wai text wai wai-extra
+    base bytestring containers doctest hedgehog hspec hspec-wai mtl
+    text wai wai-extra
   ];
   description = "Simplest of web apps";
   license = stdenv.lib.licenses.bsd3;
--- level42/tests/StateMachineTests.hs
+++ level42/tests/StateMachineTests.hs
@@ -1,14 +1,24 @@
 module StateMachineTests where
 
-import qualified Data.IntMap as M
+import qualified Data.IntMap    as M
+import           Data.Text      (Text)
 
-import           Hedgehog
-import qualified Hedgehog.Gen as Gen
+import           Hedgehog       (Command)
+import qualified Hedgehog.Gen   as Gen
 import qualified Hedgehog.Range as Range
 
+
+main :: IO ()
+main =
+  putStrLn "Hello, State Machine Tests"
+
 data Comment =
-  Comment { topic :: Text
+  Comment { topic   :: Text
           , comment :: Text
           } deriving (Eq, Show)
 
 type CommentState = M.IntMap Comment
+
+initialState :: CommentState
+initialState =
+  M.empty
