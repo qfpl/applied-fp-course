927261940c3bde2d07d3b426f39f27efdc09dbc3
Andrew McCluskey <andrew@ajmccluskey.com>
2018-02-19 11:59:06 +1000

    Get a command compiling for FirstApp

--- level42/tests/StateMachineTests.hs
+++ level42/tests/StateMachineTests.hs
@@ -1,18 +1,25 @@
+{-# LANGUAGE KindSignatures    #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module StateMachineTests where
 
 import           Control.Concurrent.Async (race_)
 import           Control.Concurrent.MVar  (MVar, newEmptyMVar, putMVar,
                                            takeMVar)
 import           Control.Monad.IO.Class   (liftIO)
+import qualified Data.ByteString.Lazy     as LBS
 import qualified Data.IntMap              as M
 import           Data.Semigroup           ((<>))
 import           Data.Text                (Text)
 import           Data.Text.IO             (hPutStrLn)
 import           GHC.Word                 (Word16)
 import           Network.Wai.Handler.Warp (run)
+import qualified Network.Wai.Test         as WT
 import           System.IO                (stderr)
 
-import           Hedgehog                 (Command)
+import           Hedgehog                 (executeSequential, Callback (..), Command (Command),
+                                           Gen, HTraversable (htraverse),
+                                           Property, assert, forAll, property)
 import qualified Hedgehog.Gen             as Gen
 import qualified Hedgehog.Range           as Range
 
@@ -42,7 +49,9 @@ data Comment =
           , comment :: Text
           } deriving (Eq, Show)
 
-type CommentState = M.IntMap Comment
+data CommentState (v :: * -> *) =
+  CommentState (M.IntMap Comment)
+  deriving (Eq, Show)
 
 env :: IO Env
 env =
@@ -55,6 +64,41 @@ env =
   in
     fmap (either splode (Env logErr c)) edb
 
-initialState :: CommentState
-initialState =
-  M.empty
+initialState :: CommentState v
+initialState = CommentState M.empty
+
+data ListTopics (v :: * -> *) =
+  ListTopics
+  deriving (Eq, Show)
+
+instance HTraversable ListTopics where
+  htraverse _ ListTopics = pure ListTopics
+
+cListTopicsEmpty :: Command Gen WT.Session CommentState
+cListTopicsEmpty =
+  let
+    gen :: CommentState v -> Maybe (Gen (ListTopics v))
+    gen (CommentState s)
+      | s == M.empty = Just (pure ListTopics)
+      | otherwise    = Nothing
+
+    execute ListTopics = do
+      rsp <- WT.request . WT.setPath WT.defaultRequest $ "/list"
+      pure $ WT.simpleBody rsp
+
+    callbacks =
+      [ Require (\(CommentState s) _i -> M.null s)
+      , Ensure (\_b _a _i -> assert . LBS.null)
+      ]
+  in
+    Command gen execute callbacks
+
+-- propFirstApp :: Property
+-- propFirstApp =
+--   property $ do
+--     env' <- liftIO env
+--     commands <- forAll $
+--       Gen.sequential (Range.linear 1 100) initialState [cListTopicsEmpty]
+--     let session = executeSequential initialState commands
+--     liftIO $ WT.runSession session (app env')
+
