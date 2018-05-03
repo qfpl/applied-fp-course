6471c42aafc6a4634ec731ab9e4129f80de1b4fe
Sean Chalmers <sclhiannan@gmail.com>
2018-05-03 13:26:15 +1000

    Fix up cabal config for doctests, missing packages and some exposed-modules

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -2,10 +2,10 @@ module Main where
 
 import Test.DocTest (doctest)
 
--- import qualified Level04Tests
--- import qualified Level05Tests
--- import qualified Level06Tests
--- import qualified Level07Tests
+import qualified Level04Tests
+import qualified Level05Tests
+import qualified Level06Tests
+import qualified Level07Tests
 
 main :: IO ()
 main = doctest [] -- No doctests yet!
