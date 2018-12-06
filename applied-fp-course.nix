73cd0fd60a8a40f70e64681c31eb951d97c6ba46
Sean Chalmers <sclhiannan@gmail.com>
2018-12-07 08:32:14 +1000

    Revert stack travis changes. Attempt to pin appar version for 7.10.3.

--- applied-fp-course.nix
+++ applied-fp-course.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, attoparsec, base, bytestring, contravariant
-, doctest, hedgehog, http-types, lens, mmorph, mtl, old-locale
+{ mkDerivation, appar, attoparsec, base, bytestring, contravariant
+, doctest, hedgehog, http-types, lens, mtl, old-locale
 , optparse-applicative, semigroups, sqlite-simple
 , sqlite-simple-errors, stdenv, tasty, tasty-hedgehog, tasty-hunit
 , tasty-wai, text, time, transformers, waargonaut, wai, wai-extra
@@ -12,14 +12,14 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    attoparsec base bytestring contravariant http-types lens mtl
+    appar attoparsec base bytestring contravariant http-types lens mtl
     old-locale optparse-applicative semigroups sqlite-simple
     sqlite-simple-errors text time transformers waargonaut wai warp
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    base bytestring doctest hedgehog http-types mmorph mtl semigroups
-    tasty tasty-hedgehog tasty-hunit tasty-wai text transformers wai
+    base bytestring doctest hedgehog http-types mtl semigroups tasty
+    tasty-hedgehog tasty-hunit tasty-wai text transformers wai
     wai-extra
   ];
   description = "Simplest of web apps for educational purposes";
