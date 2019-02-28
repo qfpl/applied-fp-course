2355f466a2f15df1d7a0aba9ab879bd2c1d83c67
Sean Chalmers <sclhiannan@gmail.com>
2019-02-28 13:20:13 +1000

    Separate out overlays
    Clean up default.nix

--- default.nix
+++ default.nix
@@ -1,46 +1,14 @@
-{ nixpkgs ? import ./nix/nixpkgs.nix # import <nixpkgs> {}
+{ nixpkgs ? import ./nix/nixpkgs.nix
 , compiler ? "default"
 }:
 let
-  sources = {
-    tasty-wai = import ./nix/tasty-wai.nix;
-    waarg = import ./nix/waargonaut.nix;
-  };
-
-  waarg-deps = import "${sources.waarg}/waargonaut-deps.nix";
-
-  course-overlay = self: super: {
-    haskellPackages = super.haskellPackages.override (old: {
-      overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (hself: hsuper: {
-        tasty-wai = hself.callCabal2nix "tasty-wai" sources.tasty-wai {};
-        waargonaut = hself.callCabal2nix "waargonaut" sources.waarg {};
-      });
-    });
-  };
-
   pkgs = import nixpkgs {
-    overlays = [ waarg-deps course-overlay ];
+    overlays = (import ./course-overlays.nix);
   };
 
   haskellPackages = if compiler == "default"
                       then pkgs.haskellPackages
                       else pkgs.haskell.packages.${compiler};
-
-  # sources = {
-  #   tasty-wai = import ./nix/tasty-wai.nix;
-  #   waarg = import ./nix/waargonaut.nix;
-  # };
-
-  # waarg-deps = import "${sources.waarg}/waargonaut-deps.nix";
-
-  # modifiedHaskellPackages = haskellPackages.override (old: {
-  #   overrides = pkgs.lib.composeExtensions
-  #     (old.overrides or (_: _: {}))
-  #     (self: super: (waarg-deps self super) // {
-  #       tasty-wai = self.callCabal2nix "tasty-wai" sources.tasty-wai {};
-  #       waargonaut = self.callCabal2nix "waargonaut" sources.waarg {};
-  #     });
-  # });
 
   drv = haskellPackages.callPackage ./applied-fp-course.nix {};
 
