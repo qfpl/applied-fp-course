97bd67c92fb4d8a9e2d31e3b8eaaff165ef13f31
Jack Kelly <jack@jackkelly.name>
2022-03-15 16:11:06 +1000

    Modernise nix somewhat
    * Provide a flake, but support `nix-shell` using flake-compat
    * Use `haskellPackages.developPackage` to call `cabal2nix` for us
    * Fix a lot of cabal file bounds to bring it into the modern age
    * Drop support for GHC <8.4.1 because I can't remember the right way
      to write the conditional `semigroups` dependency
    * Drop references to Freenode
    * Clarify some documentation

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "QFPL Applied FP Course";
+
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        nixpkgs = import inputs.nixpkgs { inherit system; };
+        applied-fp-course = { returnShellEnv ? false }:
+          nixpkgs.haskellPackages.developPackage {
+            inherit returnShellEnv;
+            root = ./.;
+
+            overrides = final: prev: with nixpkgs.haskell.lib; {
+              hw-json-simd = dontCheck (unmarkBroken prev.hw-json-simd);
+              hw-json-standard-cursor = doJailbreak prev.hw-json-standard-cursor;
+              natural = dontCheck (unmarkBroken prev.natural);
+              sqlite-simple-errors =
+                unmarkBroken (doJailbreak prev.sqlite-simple-errors);
+              waargonaut = dontCheck (doJailbreak prev.waargonaut);
+            };
+          };
+      in
+      {
+        devShell =
+          (applied-fp-course { returnShellEnv = true; }).overrideAttrs
+            (oldAttrs: {
+              buildInputs = oldAttrs.buildInputs ++ [
+                nixpkgs.cabal-install
+                nixpkgs.ghcid
+                nixpkgs.sqlite
+              ];
+            });
+      });
+}
